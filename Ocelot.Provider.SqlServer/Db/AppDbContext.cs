7c2e7aa7129f72397444d63b057dc9cb7059942e
omid <ahmadpooromid@gmail.com>
2022-02-09 23:10:03 +0330

    add ef context.

---
+++ Ocelot.Provider.SqlServer/Db/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Ocelot.Provider.SqlServer.Configuration;
+using Ocelot.Provider.SqlServer.Models;
+
+namespace Ocelot.Provider.SqlServer.Db
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly AppConfigs _appConfigs;
+
+        public AppDbContext(IOptions<AppConfigs> options)
+        {
+            _appConfigs = options.Value;
+        }
+
+        public DbSet<OcelotGlobalConfiguration> OcelotGlobalConfigurations { get; set; }
+
+        public DbSet<OcelotRoute> OcelotRoutes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_appConfigs.DbConnectionStrings);
+        }
+    }
+}
