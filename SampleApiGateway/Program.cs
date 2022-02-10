d707545de14a29793f97aef31f2c1ec45ac2b2c3
omid <ahmadpooromid@gmail.com>
2022-02-10 21:34:53 +0330

    remove extra file.

--- SampleApiGateway/Program.cs
+++ SampleApiGateway/Program.cs
@@ -28,23 +28,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseOcelotWithSqlServerProvider().Wait();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
